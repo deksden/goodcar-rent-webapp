1e4094c67ce92ecf2232a051c727dea2410bbc88
deksden <deksden@deksden.com>
2019-03-28 14:38:32 +0700

    dashboard page / component

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { UserList } from './users'
 import { PostCreate, PostEdit, PostList } from './posts'
 import PostIcon from '@material-ui/icons/Book'
 import UserIcon from '@material-ui/icons/Group'
+import Dashboard from './dashboard'
 
 const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com')
 const App = () => (
-  <Admin dataProvider={dataProvider}>
+  <Admin dashboard={Dashboard} dataProvider={dataProvider}>
     <Resource name='posts' list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
     <Resource name='users' list={UserList} icon={UserIcon} />
   </Admin>
