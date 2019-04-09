6260614c2dc975872eb829a82fcac336ad5fa984
deksden <deksden@deksden.com>
2019-04-09 14:11:57 +0700

    - (enh) WIP: adapt sample for GoodCar.rent Server

--- src/auth-provider.js
+++ src/auth-provider.js
@@ -1,29 +1,51 @@
 import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin'
+import fetch from 'node-fetch'
+
+const API_URL = 'http://localhost:3005'
 
 export default (type, params) => {
   if (type === AUTH_LOGIN) {
-    const { username } = params
-    window.localStorage.setItem('username', username)
-    // accept all username/password combinations
-    return Promise.resolve()
+    const { username, password } = params
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({ email: username, password }),
+      headers: { 'Content-Type': 'application/json' }
+    }
+
+    return fetch(`${API_URL}/auth/login`, options)
+      .then(response => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText)
+        }
+        return response.json()
+      })
+      .then(({ token }) => {
+        window.localStorage.setItem('token', token)
+        console.log(`auth ok, token ${token}`)
+      })
   }
   // called when the user clicks on the logout button
   if (type === AUTH_LOGOUT) {
-    window.localStorage.removeItem('username')
-    return Promise.resolve()
+    return fetch(`${API_URL}/auth/logout`, options)
+      .then(response => {
+        if (response.status < 200 || response.status >= 300) {
+          throw new Error(response.statusText)
+        }
+        window.localStorage.removeItem('token')
+      })
   }
   // called when the API returns an error
   if (type === AUTH_ERROR) {
     const { status } = params
     if (status === 401 || status === 403) {
-      window.localStorage.removeItem('username')
-      return Promise.reject()
+      window.localStorage.removeItem('token')
+      return Promise.reject(new Error('Auth error'))
     }
     return Promise.resolve()
   }
   // called when the user navigates to a new location
   if (type === AUTH_CHECK) {
-    return window.localStorage.getItem('username')
+    return window.localStorage.getItem('token')
       ? Promise.resolve()
       : Promise.reject(new Error('No auth'))
   }
